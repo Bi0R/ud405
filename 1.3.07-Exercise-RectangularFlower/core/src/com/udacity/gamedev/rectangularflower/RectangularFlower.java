2ce778cedcfe65f671c04427b86936b7fe99d6e5
Bi0R <bior87@gmail.com>
2016-01-01 21:07:54 -0600

    draw a rectangular flower

--- 1.3.07-Exercise-RectangularFlower/core/src/com/udacity/gamedev/rectangularflower/RectangularFlower.java
+++ 1.3.07-Exercise-RectangularFlower/core/src/com/udacity/gamedev/rectangularflower/RectangularFlower.java
@@ -40,14 +40,17 @@ public class RectangularFlower extends ApplicationAdapter {
         shapeRenderer.rectLine(100, 0, 100, 300, 20);
 
         // TODO: Draw two leaves on the stem
+        shapeRenderer.rect(100, 150, 0, 0, 40, 20, 1, 1, 150.0f);
+        shapeRenderer.rect(110, 170, 0, 0, 40, 20, 1, 1, 35);
 
         // TODO: Set the active color to yellow
+        shapeRenderer.setColor(Color.YELLOW);
 
         // TODO: Use a loop to draw 20 of these petals in a circle
-
-        float petalAngle = 45.0f;
-        shapeRenderer.rect(100, 300, 0, 0, 40, 40, 1, 1, petalAngle);
-
+        for(int i = 0; i<20; i++){
+         shapeRenderer.rect(100,320,0,0,60,30,1,1, (i*18));
+        }
+        
         shapeRenderer.end();
     }
 }
